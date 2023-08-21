8f70b8493997db8a50f543f27889550a259d3fec
anilbhangay <anilbhangay@gmail.com>
2023-08-21 23:39:57 +0530

    Completed code write in all files

--- Netflix-intro/script.js
+++ Netflix-intro/script.js
@@ -1,6 +1,7 @@
   const stick1 = document.getElementsByClassName("color")[0]
   const stick2 = document.getElementsByClassName("color2")[0]
   const stick3 = document.getElementsByClassName("brush3")[0]
+  const lines = document.getElementsByClassName("lines")[0]
 
 // Make reverse animations trigger
 
@@ -13,5 +14,57 @@ setTimeout(() => {
 }, 3500);
 
 setTimeout(() => {
-    stick1.style.animation = 'movedown 0.5s ease forwards'
+    stick1.style.animation = 'opacity 0.5s ease forwards'
+    lines.style.display = 'flex'
 }, 4000);
+
+
+// Lets create some random lines width random margins or gaps
+
+let colors = [
+    '#133286',
+    '#3062af',
+    '#FFF',
+    '#4a7fcb',
+    '#133286',
+    '#3062af',
+    '#628ace',
+    '#949fd9',
+    '#821e12',
+    '#c34821',
+    'red',
+    '#d3ad94',
+    'yellow',
+    '#821e12',
+    '#462652',
+    '#b16f67',
+    '#d3ad94',
+    '#821e12',
+    '#462652',
+    '#4a7fcb',
+    '#133286',
+    '#3062af',
+    '#628ace',
+    '#b16f67',
+    '#d3ad94',
+    'yellow',
+    '#821e12',
+    '#821e12',
+    '#c34821',
+    '#821e12',
+    '#c34821',
+    'red',
+]
+
+
+colors.map((color) => {
+   const line = document.createElement('div');
+   const randomMargin = Math.floor(Math.random() * 1000);
+
+
+   line.className = 'line';
+   line.style.setProperty('--m', `${randomMargin}px`);
+   line.style.setProperty('--c', color);
+   lines.appendChild(line);
+ 
+});
