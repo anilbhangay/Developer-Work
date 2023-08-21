554cd0893311a89db6ce395fb9b097eb4270f419
anilbhangay <anilbhangay@gmail.com>
2023-08-21 20:49:52 +0530

    Used classname in script

---
+++ Netflix-intro/script.js
@@ -0,0 +1,17 @@
+  const stick1 = document.getElementsByClassName("color")[0]
+  const stick2 = document.getElementsByClassName("color2")[0]
+  const stick3 = document.getElementsByClassName("brush3")[0]
+
+// Make reverse animations trigger
+
+setTimeout(() => {
+    stick2.style.animation = 'movedown 0.5s ease forwards'
+}, 3000);
+
+setTimeout(() => {
+     stick3.style.animation = 'movedown 0.5s ease forwards'
+}, 3500);
+
+setTimeout(() => {
+    stick1.style.animation = 'movedown 0.5s ease forwards'
+}, 4000);
