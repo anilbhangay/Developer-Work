ce4c50006d6644bca5d04282708f9eff930b268e
anilbhangay <anilbhangay@gmail.com>
2023-08-29 22:57:49 +0530

    Created navbar in navbar.jsx file

--- Payment-HooBank/Payment-HooBank/src/components/Navbar.jsx
+++ Payment-HooBank/Payment-HooBank/src/components/Navbar.jsx
@@ -9,8 +9,14 @@ const Navbar = () => {
      <nav className="w-full flex py-6 justify-between items-center navbar"> 
          <img src={logo} alt="hoobank" className="w-[124px] h-[32px]"/>
 
-         <ul className="list-none" >
+         <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+               {navLinks.map((nav, index) => (
+                 <li key={nav.id} className={`font-poppins`}
+                 >
 
+                 </li>
+
+               ))}
          </ul>
      </nav>
   )
