fff5e62b66c873f1fed9f49fcbee48c8a3cd7e16
anilbhangay <anilbhangay@gmail.com>
2023-08-29 19:58:33 +0530

    Created new navbar files in components

--- Payment-HooBank/Payment-HooBank/src/components/Navbar.jsx
+++ Payment-HooBank/Payment-HooBank/src/components/Navbar.jsx
@@ -6,7 +6,13 @@ import { navLinks } from '../constants';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+     <nav className="w-full flex py-6 justify-between items-center navbar"> 
+         <img src={logo} alt="hoobank" className="w-[124px] h-[32px]"/>
+
+         <ul className="list-none" >
+
+         </ul>
+     </nav>
   )
 }
 
