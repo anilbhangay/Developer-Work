fbc3b590a0f3c386ecb88c86b7f637d4462fb124
anilbhangay <anilbhangay@gmail.com>
2023-08-29 19:18:40 +0530

    Added components in source folder

--- Payment-HooBank/Payment-HooBank/src/App.jsx
+++ Payment-HooBank/Payment-HooBank/src/App.jsx
@@ -1,23 +1,32 @@
 import styles from "./style"
 
+import { Navbar, Hero, Stats, Business, Billing, CardDeal, Testimonials, Clients, CTA, Footer } from './components';
+
+
 const App = () =>  (
     <div className="bg-primary w-full overflow-hidden">
      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
-           Navbar
-
+           <Navbar />
         </div>
      </div>
 
       <div className={`bg-primary ${styles.flexStart}`}>
        <div className= {`${styles.boxWidth}`}>
-             Hero
+             <Hero />
        </div>
       </div>
-      
-      <div className={`bg-primary ${styles.flexStart}`}>
+
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
        <div className= {`${styles.boxWidth}`}>
-             Model
+             <Stats />
+             <Business />
+             <Billing />
+             <CardDeal />
+             <Testimonials />
+             <Clients />
+             <CTA />
+             <Footer />
        </div>
       </div>
 
