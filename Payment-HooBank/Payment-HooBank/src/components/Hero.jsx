4cd129ad29680b846371e406095745ab81f29e78
anilbhangay <anilbhangay@gmail.com>
2023-08-31 17:04:53 +0530

    Added discount logo in hero section

--- Payment-HooBank/Payment-HooBank/src/components/Hero.jsx
+++ Payment-HooBank/Payment-HooBank/src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import styles from '../style';
-// import { discount, robot } from '../assets';
+import { discount, robot } from '../assets';
 
 const Hero = () =>  (
     
@@ -7,7 +7,9 @@ const Hero = () =>  (
 
       <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
           <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
-
+              <img src={discount} alt="discount" 
+             className='w-[32px] h-[32px]' />
+              
           </div>
       </div>
     </section>  
