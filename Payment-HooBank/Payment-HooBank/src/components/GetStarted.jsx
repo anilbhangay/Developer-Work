a4654cc54c4f9a132a841d489b140e67798ce043
anilbhangay <anilbhangay@gmail.com>
2023-09-01 23:22:45 +0530

    Used styles in right side section

--- Payment-HooBank/Payment-HooBank/src/components/GetStarted.jsx
+++ Payment-HooBank/Payment-HooBank/src/components/GetStarted.jsx
@@ -2,8 +2,10 @@ import styles from "../style";
 import { arrowUp } from "../assets";
 
 const GetStarted = () => (
-    <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full`}>
+    <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}>
+      <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full `}>
 
+      </div>
     </div>
   )
 
