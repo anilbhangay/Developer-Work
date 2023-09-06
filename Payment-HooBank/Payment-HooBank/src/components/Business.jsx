461e0f92aea27439836c51dac22e2460955a38f6
anilbhangay <anilbhangay@gmail.com>
2023-09-06 23:17:53 +0530

    Created new button file in jsx

--- Payment-HooBank/Payment-HooBank/src/components/Business.jsx
+++ Payment-HooBank/Payment-HooBank/src/components/Business.jsx
@@ -8,7 +8,10 @@ const Business = () => {
        <div className={layout.sectionInfo}> 
        <h2 className={styles.heading2}>You do the business, <br className="sm:block hidden" /> we’ll handle
           the money.</h2>
-          <p></p>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>With the right credit card, you can improve your financial life by
+        building credit, earning rewards and saving money. But with hundreds
+        of credit cards on the market.</p>
+          <Button styles="mt-10"/>
        </div>
     </section>
   )
