5f913d3925c61880575bcb1b818167ae10b20f9b
anilbhangay <anilbhangay@gmail.com>
2023-09-07 22:54:03 +0530

    Added testimonials jsx in components

--- Payment-HooBank/Payment-HooBank/src/components/Billing.jsx
+++ Payment-HooBank/Payment-HooBank/src/components/Billing.jsx
@@ -8,8 +8,8 @@ const Billing = () =>  (
     <div className={layout.sectionImgReverse}>
      <img src={bill} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
 
-       <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white-gradient "/>
-       <div className="absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink-gradient "/>
+       <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient "/>
+       <div className="absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient "/>
        </div>
         <div className={layout.sectionInfo}>
            <h2 className={styles.heading2}>Easily control your <br className="sm:block hidden" /> billing & invoicing</h2>
@@ -24,4 +24,4 @@ const Billing = () =>  (
     </section>
   )
 
-export default Billing
+export default Billing;
