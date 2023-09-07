f66f22e94fcc89a9c43eabd5b331db7ef9333549
anilbhangay <anilbhangay@gmail.com>
2023-09-08 00:46:25 +0530

    Completed footer work in components

--- Payment-HooBank/Payment-HooBank/src/components/Footer.jsx
+++ Payment-HooBank/Payment-HooBank/src/components/Footer.jsx
@@ -31,13 +31,13 @@ const Footer = () => (
           </div>
       </div>
 
-         <div>
-           <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">Copyright Ⓒ 2022 HooBank. All Rights Reserved.
+         <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]">
+           <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">Copyright Ⓒ 2023 HooBank. All Rights Reserved.
            </p>
 
-             <div className="flex flex-row md:mt-0 mt-6">
+             <div className="flex flex-row md:mt-0  mt-6">
                {socialMedia.map((social, index) => (
-                    <img key={social.id} src={social.icon} alt={social.id} className=""/>
+                    <img key={social.id} src={social.icon} alt={social.id} className={`w-[21px] h-[21px] object-contain cursor-pointer ${index !== socialMedia.length -1 ? 'mr-6' : 'mr-0'}`}/>
                ))}
              </div>
          </div>
@@ -45,4 +45,4 @@ const Footer = () => (
   )
 
 
-export default Footer
+export default Footer;
