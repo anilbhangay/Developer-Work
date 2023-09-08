d81096aa28a241dd954edad741298536d5e0cd41
anilbhangay <anilbhangay@gmail.com>
2023-09-08 23:53:08 +0530

    Added social link in footer

--- Payment-HooBank/Payment-HooBank/src/components/Footer.jsx
+++ Payment-HooBank/Payment-HooBank/src/components/Footer.jsx
@@ -37,12 +37,13 @@ const Footer = () => (
 
              <div className="flex flex-row md:mt-0  mt-6">
                {socialMedia.map((social, index) => (
-                    <img key={social.id} src={social.icon} alt={social.id} className={`w-[21px] h-[21px] object-contain cursor-pointer ${index !== socialMedia.length -1 ? 'mr-6' : 'mr-0'}`}/>
+                    <img key={social.id} src={social.icon} alt={social.id} className={`w-[21px] h-[21px] object-contain cursor-pointer ${index !== socialMedia.length -1 ? 'mr-6' : 'mr-0'}`} onClick={() => window.open(social.link)}
+                    />
                ))}
              </div>
          </div>
     </section>
-  )
+  );
 
 
 export default Footer;
