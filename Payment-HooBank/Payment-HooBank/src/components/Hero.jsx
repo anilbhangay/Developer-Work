b2bab53ccc9d475ce354e85380c2a33746d31172
anilbhangay <anilbhangay@gmail.com>
2023-09-10 17:39:22 +0530

    Fixed some error in jsx

--- Payment-HooBank/Payment-HooBank/src/components/Hero.jsx
+++ Payment-HooBank/Payment-HooBank/src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import styles from '../style';
-import { discount,robot } from '../assets';
+import { discount, robot } from '../assets';
 import GetStarted from './GetStarted';
 
 const Hero = () =>  (
@@ -11,10 +11,11 @@ const Hero = () =>  (
               <img src={discount} alt="discount" 
              className='w-[32px] h-[32px]' />
                <p className={`${styles.paragraph} ml-2`}>
-               <span className='text-white'>20%</span>  Discount For { " " }
-              <span className='text-white'>1 Month</span>  Account
+               <span className='text-white'>20%</span>  Discount For {" "}
+              <span className='text-white'>1 Month</span> Account
                </p>
           </div>
+          
           <div className='flex flex-row justify-between items-center w-full'>
             <h1 className='flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]'>
                The Next <br className='sm:block hidden'/> {" "}
