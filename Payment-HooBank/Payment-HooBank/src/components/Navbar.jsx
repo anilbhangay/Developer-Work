e3b0ebfde55f11d5c00f1a2f2b498e2df8ed66c0
anilbhangay <anilbhangay@gmail.com>
2023-09-11 23:19:21 +0530

    Added active into title in navbar

--- Payment-HooBank/Payment-HooBank/src/components/Navbar.jsx
+++ Payment-HooBank/Payment-HooBank/src/components/Navbar.jsx
@@ -34,7 +34,10 @@ const Navbar = () => {
 
               <ul className="list-none flex flex-col justify-end items-center flex-1">
                {navLinks.map((nav, index) => (
-                 <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] ${index === navLinks.length - 1 ? 'mr-0' : 'mb-4'} text-white`} 
+                 <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] ${
+                  active === nav.title ? "text-white" : "text-dimWhite"
+                }
+                  ${index === navLinks.length - 1 ? 'mr-0' : 'mb-4'} text-white`} 
                  >
                    <a href={`#${nav.id}`}>
                      {nav.title}
