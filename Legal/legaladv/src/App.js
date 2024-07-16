da21afb56cae2040d5873a45acfd3f40426fa4f6
anilbhangay <anilbhangay@gmail.com>
2024-07-16 18:41:07 +0530

    Changed error code lines

--- Legal/legaladv/src/App.js
+++ Legal/legaladv/src/App.js
@@ -1,21 +1,20 @@
-import React from 'react';
-import './App.css';
-import Navbar from './components/Navbar/navbar';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import About from './components/About/about';
-import  Main from './components/Main/main';
-
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar/navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import About from "./components/About/about";
+import Main from "./components/Main/main";
 
 const App = () => {
   return (
     <Router>
       <Navbar />
       <Routes>
-      <Route path='/' element={<Main />} />
-       <Route path='/About' element={<About />} />
+        <Route path="/" element={<Main />} />
+        <Route path="/About" element={<About />} />
       </Routes>
     </Router>
-  )
-}
+  );
+};
 
 export default App;
